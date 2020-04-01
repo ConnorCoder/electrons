ffc8680748bebd7ddce03b4dc84839eff0dc9eec
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2020-04-01 11:26:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,18 +22,18 @@ function onChange() {
 
 function ePush(element) {
   let h = document.createElement("h3");
-  h.appendChild(document.createTextNode(`(${elements.indexOf(element) + 1}) ${element} - ${eConfig(elements.indexOf(element) + 1)} - ${eAbbrev(elements.indexOf(element) + 1)}`));
+  h.appendChild(document.createTextNode(`(${elements.indexOf(element) + 1}) ${element} - ${eConfig(elements.indexOf(element) + 1)} - (${eValence(elements.indexOf(element) + 1)} valence) - ${eAbbrev(elements.indexOf(element) + 1)}`));
   document.getElementById("results").appendChild(h);
 }
 
 function eConfig(atomic) {
-  let orbitals = ["1s","2s","2p","3s","3p","4s","3d","4p","5s","4d","5p","6s","4f","5d","6p","7s","5f","6d","7p"];
-  let total = [2, 4, 10, 12, 18, 20, 30, 36, 38, 48, 54, 56, 70, 80, 86, 88, 102, 112, 118];
-  let assigned = {s: 2,p: 6,d: 10,f: 14}
-  let orbits = [];
-  let highest = 0;
-  let diff = 0;
-  let index = 0;
+  let orbitals = ["1s","2s","2p","3s","3p","4s","3d","4p","5s","4d","5p","6s","4f","5d","6p","7s","5f","6d","7p"],
+      total = [2, 4, 10, 12, 18, 20, 30, 36, 38, 48, 54, 56, 70, 80, 86, 88, 102, 112, 118],
+      assigned = {s: 2,p: 6,d: 10,f: 14},
+      orbits = [],
+      highest = 0,
+      diff = 0,
+      index = 0;
   for(let x=0;x<total.length;x++) {
     if(atomic > total[x]) {
       highest = total[x + 1];
@@ -43,9 +43,53 @@ function eConfig(atomic) {
     }
   }
 
-  return ((atomic > 2) ? `[${orbits.join(" ")} ${orbitals[index]}${diff}]`:`[1s${atomic}]`);
+  if (atomic > 2) {
+    orbits.push(`${orbitals[index]}${diff}`);
+  }else {
+    orbits.push(`1s${atomic}`);
+  }
+
+  return orbits.join(" ");
 }
 
 function eAbbrev(atomic) {
   return "";
+}
+
+function eValence(atomic) {
+  let orbitals = ["1s","2s","2p","3s","3p","4s","3d","4p","5s","4d","5p","6s","4f","5d","6p","7s","5f","6d","7p"],
+      total = [2, 4, 10, 12, 18, 20, 30, 36, 38, 48, 54, 56, 70, 80, 86, 88, 102, 112, 118],
+      assigned = {s: 2,p: 6,d: 10,f: 14},
+      orbits = [],
+      value = [],
+      highest = 0,
+      diff = 0,
+      index = 0;
+  for(let x=0;x<total.length;x++) {
+    if(atomic > total[x]) {
+      highest = total[x + 1];
+      index = x + 1;
+      diff = atomic - total[x];
+      orbits.push(orbitals[x] + assigned[orbitals[x][orbitals[x].length - 1]]);
+    }
+  }
+
+  if (atomic > 2) {
+    orbits.push(`${orbitals[index]}${diff}`);
+  }else {
+    orbits.push(`1s${atomic}`);
+  }
+
+  let max = 0;
+  let applicable = 0;
+  for(let x=0;x<orbits.length;x++) {
+    if (parseInt(orbits[x].slice(0, 1)) > max) {
+      max = parseInt(orbits[x].slice(0, 1));
+      applicable = parseInt(orbits[x].slice(2, 3));
+    }else if(parseInt(orbits[x].slice(0, 1)) === max) {
+      applicable += parseInt(orbits[x].slice(2, 3));
+    }
+  }
+
+  return applicable;
 }
